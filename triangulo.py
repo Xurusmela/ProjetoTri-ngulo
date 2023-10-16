0e1565f8688d9511c6434365ea8702f6d0f82b90
BiaMayumi <109619902+BiaMayumi@users.noreply.github.com>
2023-10-16 16:12:04 -0300

    Validação do Triângulo

---
+++ triangulo.py
@@ -0,0 +1,12 @@
+# Validation um triangulo
+
+# Entrada
+a = float(input('Digite o valor do lado a: '))
+b = float(input('Digite o valor do lado b: '))
+c = float(input('Digite o valor do lado c: '))
+
+# Processamento e Saída
+if (b - c) < a < b + c and (a - c) < b < a + c and (a - b) < c < a + b:
+    print(f'{a}, {b}, {c}, formam um triângulo.')
+else:
+    print(f'{a}, {b}, {c}, não formam um triângulo.')
